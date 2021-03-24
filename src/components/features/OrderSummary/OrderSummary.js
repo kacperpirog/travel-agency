26bb0408c15bec08e8fb1492790f6b30200a8842
kacper pirog <kacper.pirog@gmail.com>
2021-03-24 12:24:43 +0100

    fixOderForm watch trip_1

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import styles from './OrderSummary.scss';
-import PropTypes from 'prop-types';
-import { calculateTotal } from '../../../utils/calculateTotal';
-import { formatPrice } from '../../../utils/formatPrice';
+import React from "react";
+import styles from "./OrderSummary.scss";
+import PropTypes from "prop-types";
+import { calculateTotal } from "../../../utils/calculateTotal";
+import { formatPrice } from "../../../utils/formatPrice";
 
 const OrderSummary = (props) => {
   const { tripCost, options } = props;
   return (
-    <h2 className={styles.component}>Total: <strong>{formatPrice(calculateTotal(tripCost, options))}</strong></h2>
+    <h2 className={styles.component}>
+      Total: <strong>{formatPrice(calculateTotal(tripCost, options))}</strong>
+    </h2>
   );
 };
 
